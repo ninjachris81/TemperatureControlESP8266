4ee42e7d9e5edbb660a1044152ea26249376beef
ninjachris81 <cbstar@web.de>
2016-02-12 20:34:15 +0100

    corrected http_utils

---
+++ Shared/http_utils.cpp
@@ -0,0 +1,39 @@
+#include "http_utils.h"
+
+bool HttpUtils::waitUntilConnected(HTTPClient &httpClient) {
+  uint8_t timeout = 0;
+  while(!httpClient.connected()) {
+    delay(250);
+    timeout++;
+    if (timeout>20) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int HttpUtils::executeGET(String host, int port, String query) {
+  HTTPClient httpClient;
+  httpClient.setUserAgent("TempControl");
+  
+  httpClient.begin(host, port, query); //HTTP
+  HttpUtils::waitUntilConnected(httpClient);
+  int returnCode = httpClient.GET();
+  httpClient.end();
+  return returnCode;
+}
+
+String HttpUtils::executeGET(String host, int port, String query, int &returnCode, int &contentSize) {
+  HTTPClient httpClient;
+  httpClient.setUserAgent("TempControl");
+
+  httpClient.begin(host, port, query); //HTTP
+  HttpUtils::waitUntilConnected(httpClient);
+  returnCode = httpClient.GET();
+  contentSize = httpClient.getSize();
+  String content = httpClient.getString();
+  httpClient.end();
+  return content;
+}
+
+
---
+++ Shared/http_utils.h
@@ -0,0 +1,20 @@
+#ifndef HTTP_UTILS_H
+#define HTTP_UTILS_H
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+
+class HttpUtils {
+public:
+  static bool waitUntilConnected(HTTPClient &httpClient);
+
+  static int executeGET(String host, int port, String query);
+
+  static String executeGET(String host, int port, String query, int &returnCode, int &contentSize);
+
+};
+
+
+#endif
+
